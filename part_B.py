5c4dfa9b0b243508cf2efb7b566ca54ec834f3cf
Lila P Fageraas <lilapfageraas@Lilas-Air.home>
2024-10-02 11:54:38 +0200

    adding my solution to part B of the assignment

---
+++ part_B.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 10:20:21 2024
+
+@author: lilapfageraas
+"""
+
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# TECH 2 mandatory assignment - Part B\n",
+    "\n",
+    "Write your solution for Part B in this notebook."
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
